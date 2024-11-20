3efeaa123f73dbaed6c19df935e5247bf4a071c6
Florian Wicher <florian.wicher@rise-world.com>
2024-11-20 15:58:41 +0100

    Demo 2: add validation, types

--- src/money_transfer.py
+++ src/money_transfer.py
@@ -1,6 +1,8 @@
 accounts = {}
 
-def create_account(account_name, amount):
+def create_account(account_name:str, amount:float):
+    if amount < 0:
+        raise ValueError("Initial balance cannot be negative.")
     account = {
         'balance': amount
     }
@@ -9,10 +11,17 @@ def create_account(account_name, amount):
     return account
 
 
-def transfer_money(from_account_name, amount, to_account_name):
+def transfer_money(from_account_name:str, amount:float, to_account_name:str):
+    if amount <= 0:
+        raise ValueError("Transfer amount must be greater than zero.")
+
     from_account = accounts[from_account_name]
     to_account = accounts[to_account_name]
 
+    if from_account is None:
+        raise ValueError(f"Account '{from_account_name}' not found.")
+    if to_account is None:
+        raise ValueError(f"Account '{to_account_name}' not found.")
     if from_account['balance'] < amount:
         raise ValueError(f"Insufficient funds in '{from_account_name}'.")
 
@@ -20,9 +29,12 @@ def transfer_money(from_account_name, amount, to_account_name):
     to_account['balance'] += amount
     print(f"Transferred {amount} from '{from_account_name}' to '{to_account_name}'.")
 
-def show_account(account_name):
+def show_account(account_name:str):
     account = accounts.get(account_name)
-    print(f"Account '{account_name}': Balance = {account['balance']}")
+    if account:
+        print(f"Account '{account_name}': Balance = {account['balance']}")
+    else:
+        print(f"Account '{account_name}' not found.")
 
 if __name__ == "__main__":
     create_account("Alice", 100)
